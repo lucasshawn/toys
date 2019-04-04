e13d219acca52d1390933fa0e591d849a9418a79
Shawn Lucas <shawn.lucas@oracle.com>
2019-04-04 09:04:05 -0700

    Added base64 source

---
+++ src/Base64/Base64Encoder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Base64
+{
+    class Base64FileEncoder
+    {
+        public void Encode(string inFileName, string outFileName)
+        {
+            if (outFileName != null)
+            {
+                using (System.IO.FileStream outStream = System.IO.File.Create(outFileName))
+                {
+                    Encode(inFileName, outStream);
+                }
+            }
+            else
+            {
+                using (Stream outStream = Console.OpenStandardOutput())
+                {
+                    Encode(inFileName, outStream);
+                }
+
+            }
+        }
+        public void Decode(string inFileName, string outFileName)
+        {
+            if (outFileName != null)
+            {
+                using (System.IO.FileStream outStream = System.IO.File.Create(outFileName))
+                {
+                    Decode(inFileName, outStream);
+                }
+            }
+            else
+            {
+                using (Stream outStream = Console.OpenStandardOutput())
+                {
+                    Decode(inFileName, outStream);
+                }
+
+            }
+        }
+        private void Encode(string inFileName, Stream outFile)
+        {
+            System.Security.Cryptography.ICryptoTransform transform = new System.Security.Cryptography.ToBase64Transform();
+            using (System.IO.FileStream inFile = System.IO.File.OpenRead(inFileName))
+            {
+                
+                using (System.Security.Cryptography.CryptoStream cryptStream = new System.Security.Cryptography.CryptoStream(outFile, transform, System.Security.Cryptography.CryptoStreamMode.Write))
+                {
+                    // I'm going to use a 4k buffer, tune this as needed
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                    while ((bytesRead = inFile.Read(buffer, 0, buffer.Length)) > 0)
+                        cryptStream.Write(buffer, 0, bytesRead);
+
+                    cryptStream.FlushFinalBlock();
+                }
+            }
+        }
+
+        private void Decode(string inFileName, Stream outFile)
+        {
+            System.Security.Cryptography.ICryptoTransform transform = new System.Security.Cryptography.FromBase64Transform();
+            using (System.IO.FileStream inFile = System.IO.File.OpenRead(inFileName))
+            {
+
+                using (System.Security.Cryptography.CryptoStream cryptStream = new System.Security.Cryptography.CryptoStream(inFile, transform, System.Security.Cryptography.CryptoStreamMode.Read))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                    while ((bytesRead = cryptStream.Read(buffer, 0, buffer.Length)) > 0)
+                        outFile.Write(buffer, 0, bytesRead);
+
+                    outFile.Flush();
+                }
+            }
+        }
+
+        // this version of Encode pulls everything into memory at once
+        // you can compare the output of my Encode method above to the output of this one
+        // the output should be identical, but the crytostream version
+        // will use way less memory on a large file than this version.
+        public void MemoryEncode(string inFileName, string outFileName)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(inFileName);
+            System.IO.File.WriteAllText(outFileName, System.Convert.ToBase64String(bytes));
+        }
+    }
+}
---
+++ src/Base64/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Base64
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string srcFile = null;
+            string dstFile = null;
+            Action Action = Action.None;
+
+        //    Directory.SetCurrentDirectory
+            foreach (string s in args)
+            {
+                if (s.StartsWith("-") || s.StartsWith("/"))
+                {
+                    string sAction = s.Substring(1).ToUpper();
+                    if (sAction.StartsWith("H"))
+                    {
+                        ShowHelp();
+                        return 0;
+                    }
+                    if (Action == Program.Action.None)
+                    {
+                        if (sAction.StartsWith("E"))
+                            Action = Action.Encode;
+                        else if (sAction.StartsWith("D"))
+                            Action = Action.Decode;
+                        else
+                            return ExitWithError(ErrorCode.invalidSwitch, s);
+                    }
+                    else
+                        return ExitWithError(ErrorCode.tooManySwitches, s);
+
+                }
+                else  //must be file
+                {
+                    string aFile = s;
+                    if (aFile.StartsWith("'"))  aFile = aFile.Substring(1);
+                    if (aFile.StartsWith("\"")) aFile = aFile.Substring(1);
+                    if (aFile.EndsWith("'")) aFile = aFile.Substring(0, aFile.Length - 1);
+                    if (aFile.EndsWith("\"")) aFile = aFile.Substring(0, aFile.Length - 1);
+
+                    if (srcFile == null)  //that's the first file
+                    {
+                        if (!File.Exists(aFile))
+                            return ExitWithError(ErrorCode.srcFileDoesntExist, s);
+                        else
+                            srcFile = aFile;
+                    }
+                    else if (dstFile == null)
+                    {
+                        dstFile = aFile;
+                    }
+                    else
+                        return ExitWithError(ErrorCode.tooManyParameters, s);
+
+                }
+            }
+            if (Action == Action.None)
+                return ExitWithError(ErrorCode.noActionDefined, "");
+
+            if (srcFile == null)
+                return ExitWithError(ErrorCode.noSrcFileProvided, "");
+
+            //Do stuff here
+            switch (Action)
+            {
+                case Program.Action.Encode:
+                    try
+                    {
+                        Base64FileEncoder BE = new Base64FileEncoder();
+                        BE.Encode(srcFile, dstFile);
+                        if (dstFile!=null)
+                            Console.WriteLine(string.Format("File '{0}' was successfully Byte64 encoded and saved to '{1}'", srcFile, dstFile));
+                    }
+                    catch (Exception ex)
+                    {
+                        return ExitWithError(ErrorCode.errorWhileEncoding, ex.Message);
+                    }
+                    break;
+                case Program.Action.Decode:
+                    try
+                    {
+                        Base64FileEncoder BE = new Base64FileEncoder();
+                        BE.Decode(srcFile, dstFile);
+                        if (dstFile != null)
+                            Console.WriteLine(string.Format("File '{0}' was successfully decoded and saved to '{1}'", srcFile, dstFile));
+                    }
+                    catch (Exception ex)
+                    {
+                        return ExitWithError(ErrorCode.errorWhileDecoding, ex.Message);
+                    }
+                    break;
+            }
+
+            return 1;
+
+
+        }
+
+        static int ExitWithError(ErrorCode errorCode, string Arg)
+        {
+            switch (errorCode)
+            {
+                case ErrorCode.srcFileDoesntExist:
+                    Console.WriteLine(string.Format("srcFile: '{0}' doesn't exist", Arg));
+                    break;
+                case ErrorCode.invalidSwitch:
+                    Console.Write(string.Format("Invalid switch:{0}", Arg));
+                    break;
+                case ErrorCode.tooManySwitches:
+                    Console.WriteLine("Only one switch allowed /e or /d or /h");
+                    break;
+                case ErrorCode.tooManyParameters:
+                    Console.WriteLine("Too many parameters");
+                    break;
+                case ErrorCode.noSrcFileProvided:
+                    Console.WriteLine("No srcFile provided");
+                    break;
+                case ErrorCode.noActionDefined:
+                    Console.WriteLine("No action defined; expected one of the following: /e or /d or /h");
+                    break;
+
+
+            }
+            Console.WriteLine();
+            ShowHelp();
+            return 0;
+
+        }
+        private static void ShowHelp()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Base64 by ProXoft L.L.C (http://www.proxoft.com)");
+            Console.WriteLine("");
+ 
+            Console.WriteLine("Valid command line syntax:");
+            Console.WriteLine("To Encode:");
+            Console.WriteLine("Base64[.exe] /e[ncode] sourceFile [destinationFile]");
+            Console.WriteLine("To Decode:");
+
+            Console.WriteLine("Base64[.exe] /d[ecode] sourceFile [destinationFile]");
+            Console.WriteLine("For Help:");
+
+            Console.WriteLine("Base64[.exe] /h");
+            Console.WriteLine();
+            Console.WriteLine("Note:");
+            Console.WriteLine("if optional parameter [destinationFile] is omitted, program's output will be redirected to the console window.");
+
+        }
+
+        enum ErrorCode
+        {
+            None,
+            noActionDefined,
+            srcFileDoesntExist,
+            invalidSwitch,
+            tooManySwitches,
+            tooManyParameters,
+            noSrcFileProvided,
+            errorWhileEncoding,
+            errorWhileDecoding
+
+        }
+        enum Action
+        {
+            None,
+            Encode,
+            Decode
+        }
+    }
+}
